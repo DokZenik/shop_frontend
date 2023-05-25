f3d90f4d41a34a80788ddd002e372e462a35efbf
Danil Zinoveev <dzinoveev@gmail.com>
2023-05-25 16:30:50 +0300

    first commit

--- src/components/homeComponents/ShopSection.jsx
+++ src/components/homeComponents/ShopSection.jsx
@@ -1,55 +1,58 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import Rating from "./Rating";
 import Pagination from "./pagination";
 import products from "../../data/Products";
 
 const ShopSection = () => {
-  return (
-    <>
-      <div className="container">
-        <div className="section">
-          <div className="row">
-            <div className="col-lg-12 col-md-12 article">
-              <div className="shopcontainer">
-                {products.map((product) => (
-                  <div
-                    className="shop"
-                    key={product._id}
-                  >
+    return (
+        <>
+            <div className="container">
+                <div className="section">
+                    <div className="row">
+                        <div className="col-lg-12 col-md-12 article">
+                            <div className="shopcontainer">
+                                {products.map((product) => (
+                                    <div
+                                        className="shop"
+                                        key={product._id}
+                                    >
 
-                      <Link to={`/products/${product._id}`}>
-                        <div className="shopBack">
-                          <img src={product.image} alt={product.name} />
+                                        <Link to={`/products/${product._id}`}>
+                                            <div className="shopBack">
+                                                <img src={product.image} alt={product.name}/>
+                                            </div>
+                                        </Link>
+
+                                        <div className="shoptext">
+                                            <p>
+                                                <Link to={`/products/${product._id}`}>
+                                                    {product.name}
+                                                </Link>
+                                            </p>
+                                        </div>
+
+                                        <div className="item__rating">
+                                            <Rating
+                                                value={product.rating}
+                                                text={`${product.numReviews} reviews`}
+                                            />
+                                        </div>
+                                        <div className="item_price">
+                                            <h3>${product.price}</h3>
+                                        </div>
+
+                                        {/*</div>*/}
+                                    </div>
+                                ))}
+                            </div>
+                            <Pagination/>
                         </div>
-                      </Link>
-
-                      <div className="shoptext">
-                        <p>
-                          <Link to={`/products/${product._id}`}>
-                            {product.name}
-                          </Link>
-                        </p>
-                      </div>
-
-                      <div className="item__rating">
-                        <Rating
-                          value={product.rating}
-                          text={`${product.numReviews} reviews`}
-                        />
-                        <h3>${product.price}</h3>
-                      </div>
-                    {/*</div>*/}
-                  </div>
-                ))}
-              </div>
-              <Pagination />
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 };
 
 export default ShopSection;
