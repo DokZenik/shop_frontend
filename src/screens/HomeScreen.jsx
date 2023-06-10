bbf73e9b14886d80b3099f8c5e8c7e92a1c2c051
Philyaa3 <psychevskiy@gmail.com>
2023-06-10 23:01:48 +0300

    ?

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,29 +1,29 @@
-import React, {useState} from 'react';
-import Header from './../components/Header';
-import ShopSection from './../components/homeComponents/ShopSection';
-import ContactInfo from './../components/homeComponents/ContactInfo';
-import CalltoActionSection from './../components/homeComponents/CalltoActionSection';
-import Footer from './../components/Footer';
-import ModalCart from "../components/utils/Cart/ModalCart";
-import Navigation from "./Navigation";
-import PreFooter from "./PreFooter";
-
-const HomeScreen = () => {
-    window.scrollTo(0, 0);
-    const [modal, setModal] = useState(false)
-    const [filteredItems, setFilteredItems] = useState([]);
-
-    return (
-        <div>
-            <Header setVisible={setModal} cartEnable={true} filteredItems={filteredItems} setFilteredItems={setFilteredItems} filterEnable={true}/>
-            <Navigation/>
-            <ShopSection visible={modal} setVisible={setModal} filteredItems={filteredItems} setFilteredItems={setFilteredItems}/>
-            <CalltoActionSection/>
-            <ContactInfo/>
-            <PreFooter/>
-            <Footer/>
-        </div>
-    );
-};
-
-export default HomeScreen;
+// import React, {useState} from 'react';
+// import Header from './../components/Header';
+// import ShopSection from './../components/homeComponents/ShopSection';
+// import ContactInfo from './../components/homeComponents/ContactInfo';
+// import CalltoActionSection from './../components/homeComponents/CalltoActionSection';
+// import Footer from './../components/Footer';
+// import ModalCart from "../components/utils/Cart/ModalCart";
+// import Navigation from "./Navigation";
+// import PreFooter from "./PreFooter";
+//
+// const HomeScreen = () => {
+//     window.scrollTo(0, 0);
+//     const [modal, setModal] = useState(false)
+//     const [filteredItems, setFilteredItems] = useState([]);
+//
+//     return (
+//         <div>
+//             <Header setVisible={setModal} cartEnable={true} filteredItems={filteredItems} setFilteredItems={setFilteredItems} filterEnable={true}/>
+//             <Navigation/>
+//             <ShopSection visible={modal} setVisible={setModal} filteredItems={filteredItems} setFilteredItems={setFilteredItems}/>
+//             <CalltoActionSection/>
+//             <ContactInfo/>
+//             <PreFooter/>
+//             <Footer/>
+//         </div>
+//     );
+// };
+//
+// export default HomeScreen;
