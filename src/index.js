58525db3febe57759203bb5cdcc6667dba599e0b
Danil Zinoveev <dzinoveev@gmail.com>
2023-06-24 16:27:02 +0300

    test for deploy with server

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {store} from './Redux/store';
 import {Provider} from 'react-redux';
-import {disableReactDevTools} from "@fvilers/disable-react-devtools";
+// import {disableReactDevTools} from "@fvilers/disable-react-devtools";
 
-if(process.env.NODE_ENV === 'production') disableReactDevTools()
+// if(process.env.NODE_ENV === 'production') disableReactDevTools()
 ReactDOM.render(
     <Provider store={store}>
         <App/>
