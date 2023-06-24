63ec345dbe6889b2339a49f4563da76567d0b7c3
Danil Zinoveev <dzinoveev@gmail.com>
2023-06-24 14:25:42 +0300

    test for deploy

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { store } from './Redux/store';
-import { Provider } from 'react-redux';
+import {store} from './Redux/store';
+import {Provider} from 'react-redux';
+import {disableReactDevTools} from "@fvilers/disable-react-devtools";
 
-
+if(process.env.NODE_ENV === 'production') disableReactDevTools()
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+    <Provider store={store}>
+        <App/>
+    </Provider>,
 
-  document.getElementById('root'),
+    document.getElementById('root'),
 );
